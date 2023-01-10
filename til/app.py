338ffdd6b22128c3a6f34c6735a6163dadda10b1
HEUKWU <yoo4068@gmail.com>
2023-01-10 12:04:08 +0900

    no message

--- til/app.py
+++ til/app.py
@@ -29,28 +29,23 @@ def til_get():
     til_list = list(db.til.find({},{'_id':False}))
     return jsonify({'tils': til_list})
 
+@app.route("/ranking", methods=["GET"])
+def rank_get():
+    rank_list = list(db.til.find({},{'_id':False}))
+    return jsonify({'tils': rank_list})
+
 @app.route("/til", methods=["POST"])
 def blog_post():
     name_receive = request.form['name_give']
-    vlog_url_receive = request.form['vlog_url_give']
-    title_receive = request.form['title_give']
-    img_receive = request.form['img_give']
-    comment_receive = request.form['comment_give']
-    desc_receive = request.form['desc_give']
+    address_receive = request.form['address_give']
     count = list(db.til.find({},{'_id':False}))
     num = len(count) + 1
-    comment5_receive = request.form['comment5_give']
 
     doc = {
         'name': name_receive,
-        'vlog_url': vlog_url_receive,
-        'title': title_receive,
-        'img': img_receive,
-        'comment5': comment5_receive,
-        'desc_receive': desc_receive,
-        'like_num': num,
-        'like': 0,
-        'comment5': comment5_receive
+        'address': address_receive,
+        'num': num,
+        'like': 0
     }
 
     db.til.insert_one(doc)
@@ -59,10 +54,10 @@ def blog_post():
 
 @app.route("/like", methods=["POST"])
 def like():
-    like_receive = request.form['like_give']
+    num_receive = request.form['num_give']
 
-    db.til.update_one({'like_num': int(like_receive)}, {'$inc': {'like': 1}})
+    db.til.update_one({'num': int(num_receive)}, {'$inc': {'like': 1}})
     return jsonify({'msg': '좋아요 완료!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5002, debug=True)
+    app.run('0.0.0.0', port=5003, debug=True)
