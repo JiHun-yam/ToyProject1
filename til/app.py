160fc50442a94b106f5e65baba864c694d61b076
hdlee0619 <dlgusehd1211@naver.com>
2023-01-10 10:43:26 +0900

    API 명세서 JSON 변수명 추가 및 수정

--- til/app.py
+++ til/app.py
@@ -32,14 +32,22 @@ def til_get():
 @app.route("/til", methods=["POST"])
 def blog_post():
     name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
+    vlog_url_receive = request.form['vlog_url_give']
+    title_receive = request.form['title_give']
+    img_receive = request.form['img_give']
+    comment_receive = request.form['comment_give']
+    desc_receive = request.form['desc_give']
     count = list(db.til.find({},{'_id':False}))
     num = len(count) + 1
 
     doc = {
         'name': name_receive,
-        'address': address_receive,
-        'num': num,
+        'vlog_url': vlog_url_receive,
+        'title': title_receive,
+        'img': img_receive,
+        'comment': comment_receive,
+        'desc_receive': desc_receive,
+        'like_num': num,
         'like': 0
     }
 
@@ -49,9 +57,9 @@ def blog_post():
 
 @app.route("/like", methods=["POST"])
 def like():
-    num_receive = request.form['num_give']
+    like_receive = request.form['like_give']
 
-    db.til.update_one({'num': int(num_receive)}, {'$inc': {'like': 1}})
+    db.til.update_one({'like_num': int(like_receive)}, {'$inc': {'like': 1}})
     return jsonify({'msg': '좋아요 완료!'})
 
 if __name__ == '__main__':
